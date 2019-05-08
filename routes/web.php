f90fd146b4b908d682257a5f86a690a5479fecf3
root <email.israel.br@gmail.com>
2019-05-08 17:43:54 -0400

    novas rotas

--- routes/web.php
+++ routes/web.php
@@ -24,24 +24,24 @@ Route::get('/atividade/atualizar/{id}', 'AtividadeController@update');
 
 Route::get('/meta', 'MetaController@index');
 Route::get('/meta/criar', 'MetaController@create');
-Route::get('/meta/{id}', 'MetaController@index');
+Route::get('/meta/{id}', 'MetaController@show');
 Route::post('/meta/delete/{id}', 'MetaController@destroy');
 Route::get('/meta/atualizar/{id}', 'MetaController@update');
 
 Route::get('/diaria', 'DiariaController@index');
 Route::get('/diaria/criar', 'DiariaController@create');
-Route::get('/diaria/{id}', 'DiariaController@index');
+Route::get('/diaria/{id}', 'DiariaController@show');
 Route::post('/diaria/delete/{id}', 'DiariaController@destroy');
 Route::post('/diaria/atualizar/{id}', 'DiariaController@update');
 
 Route::get('/semanal', 'SemanalController@index');
 Route::get('/semanal/criar', 'SemanalController@create');
-Route::get('/semanal/{id}', 'SemanalController@index');
+Route::get('/semanal/{id}', 'SemanalController@show');
 Route::post('/semanal/delete/{id}', 'SemanalController@destroy');
 Route::post('/semanal/atualizar/{id}', 'SemanalController@update');
 
 Route::get('/mensal', 'MensalController@index');
 Route::get('/mensal/criar', 'MensalController@create');
-Route::get('/mensal/delete/{id}', 'MensalController@index');
+Route::get('/mensal/{id}', 'MensalController@show');
 Route::post('/mensal/delete/{id}', 'MensalController@destroy');
 Route::post('/mensal/atualizar/{id}', 'MensalController@update');

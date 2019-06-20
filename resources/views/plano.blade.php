f39cbf02960bba1060b0a339052356c9e250b561
israelmarmar <email.israel.br@gmail.com>
2019-06-20 17:08:03 -0400

    fix https

--- resources/views/plano.blade.php
+++ resources/views/plano.blade.php
@@ -214,10 +214,10 @@
 		});
 	</script>
 
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/skel.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>
+	<script src="{{ URL::asset('assets/js/jquery.min.js')}}"></script>
+	<script src="{{ URL::asset('assets/js/skel.min.js')}}"></script>
+	<script src="{{ URL::asset('assets/js/util.js')}}"></script>
+	<script src="{{ URL::asset('assets/js/main.js')}}"></script>
 
 </body>
 

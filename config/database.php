d9cc4ce754549a802d6c516b668e2f7c2236e91a
israelmarmar <email.israel.br@gmail.com>
2019-06-20 16:06:37 -0400

    fix https

--- config/database.php
+++ config/database.php
@@ -42,7 +42,6 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'options' => [PDO::ATTR_EMULATE_PREPARES => true,],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -54,9 +53,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
